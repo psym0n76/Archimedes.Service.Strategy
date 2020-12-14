01fb36db6862f7b24cf891392557bcd87d39de6b
Simon Burton <burt76@outlook.com>
2020-12-14 13:16:00 +0000

    changed strategy loop

--- Archimedes.Service.Strategy/Subscribers/StrategySubscriber.cs
+++ Archimedes.Service.Strategy/Subscribers/StrategySubscriber.cs
@@ -6,8 +6,10 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using Archimedes.Library.Candles;
 using Archimedes.Library.Logger;
 using Archimedes.Library.Message.Dto;
@@ -59,60 +61,55 @@ namespace Archimedes.Service.Strategy
             try
             {
                 var strategies = await _client.GetStrategiesByGranularityMarket(message.Market, message.Granularity);
-                _batchLog.Update(_logId, $"Loaded {strategies.Count} strategies against {message.Market} {message.Granularity}" );
+                _batchLog.Update(_logId,
+                    $"Loaded {strategies.Count} strategies against {message.Market} {message.Granularity}");
 
                 if (!strategies.Any())
                 {
-                    _batchLog.Update(_logId,"No Active strategies found");
+                    _batchLog.Update(_logId, "No Active strategies found");
                     _logger.LogInformation(_batchLog.Print(_logId));
                     return;
                 }
 
-                var marketCandles = await _client.GetCandlesByGranularityMarket(message.Market, message.Granularity);
-                _batchLog.Update(_logId,
-                    $"Loaded {marketCandles.Count} MarketCandles");
-
-
-                var candles = _loader.Load(marketCandles);
-                _batchLog.Update(_logId, $"Loaded {candles.Count} Candles in CandleLoader");
-
                 foreach (var strategy in strategies)
                 {
-                    if (strategy.Active)
+                    var candles = await LoadCandles(message, strategy.EndDate);
+
+                    _batchLog.Update(_logId, $"Starting strategy {strategy.Name}");
+
+                    //var levels =
+                    //    _priceLevelStrategy.Calculate(
+                    //        candles.Where(a => a.TimeStamp >= strategy.EndDate).ToList(), 7);
+
+                    var levels = _priceLevelStrategy.Calculate(candles, 7);
+
+                    _batchLog.Update(_logId, $"PriceLevels response {levels.Count}");
+
+                    if (!levels.Any())
                     {
-                        _batchLog.Update(_logId, $"Starting strategy {strategy.Name}");
-
-                        var levels =
-                            _priceLevelStrategy.Calculate(
-                                candles.Where(a => a.TimeStamp >= strategy.EndDate).ToList(), 7);
-
-                        _batchLog.Update(_logId, $"PriceLevels response {levels.Count}");
-
-                        if (!levels.Any())
-                        {
-                            continue;
-                        }
-
-                        {
-                            _batchLog.Update(_logId, $"Publish to ArchimedesPriceLevels");
-                            _producerFanout.PublishMessages(levels, "Archimedes_Price_Level");
-
-                            _batchLog.Update(_logId, $"Post PriceLevels to Repo API");
-                            _client.AddPriceLevel(levels);
-
-                            strategy.EndDate = levels.Max(a => a.TimeStamp);
-                            //strategy.StartDate = levels.Min(a => a.TimeStamp);
-                            strategy.Count = levels.Count;
-                            strategy.LastUpdated = DateTime.Now;
-
-                            _batchLog.Update(_logId, $"Update StrategyMetrics to Repo API");
-                            _client.UpdateStrategyMetrics(strategy);
-
-                            _batchLog.Update(_logId, $"Publish StrategyMetrics to Hub");
-                            await _context.Clients.All.SendAsync("Update", strategy);
-                            _batchLog.Update(_logId, $"Ending strategy {strategy.Name}");
-                        }
+                        continue;
                     }
+
+                    {
+                        _batchLog.Update(_logId, $"Publish to ArchimedesPriceLevels");
+                        _producerFanout.PublishMessages(levels, "Archimedes_Price_Level");
+
+                        _batchLog.Update(_logId, $"Post PriceLevels to Repo API");
+                        _client.AddPriceLevel(levels);
+
+                        strategy.EndDate = levels.Max(a => a.TimeStamp);
+                        //strategy.StartDate = levels.Min(a => a.TimeStamp);
+                        strategy.Count = levels.Count;
+                        strategy.LastUpdated = DateTime.Now;
+
+                        _batchLog.Update(_logId, $"Update StrategyMetrics to Repo API");
+                        _client.UpdateStrategyMetrics(strategy);
+
+                        _batchLog.Update(_logId, $"Publish StrategyMetrics to Hub");
+                        await _context.Clients.All.SendAsync("Update", strategy);
+                        _batchLog.Update(_logId, $"Ending strategy {strategy.Name}");
+                    }
+
                 }
 
                 _logger.LogInformation(_batchLog.Print(_logId));
@@ -121,6 +118,22 @@ namespace Archimedes.Service.Strategy
             {
                 _logger.LogError($"Unable to Update Market Metrics message {e.Message} {e.StackTrace}");
             }
+        }
+
+        private async Task<List<Candle>> LoadCandles(StrategyMessage message, DateTime endDate)
+        {
+            //var marketCandles = await _client.GetCandlesByGranularityMarket(message.Market, message.Granularity);
+            var marketCandles = await _client.GetCandlesByGranularityMarketByDate(message.Market, message.Granularity,
+                endDate, DateTime.Now);
+
+
+            _batchLog.Update(_logId,
+                $"Loaded {marketCandles.Count} MarketCandles");
+
+
+            var candles = _loader.Load(marketCandles);
+            _batchLog.Update(_logId, $"Loaded {candles.Count} Candles in CandleLoader");
+            return candles;
         }
     }
 }
